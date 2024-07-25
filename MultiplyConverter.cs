938a7066fb0831c75dd104c1292df7f57675bc31
Daniel Koman <dankoman30@gmail.com>
2024-07-25 12:49:01 -0700

    playing with reflection effect

---
+++ MultiplyConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfApp1
+{
+    public class MultiplyConverter : IValueConverter
+    {
+        public double Factor { get; set; } = 1.0;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double doubleValue)
+            {
+                return doubleValue * Factor;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
