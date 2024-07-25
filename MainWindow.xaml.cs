5c98ccbba5469791186475ec144e488f065bed5d
Daniel Koman <dankoman30@gmail.com>
2024-07-25 09:22:40 -0700

    close but not quite right
    can you run this through chat to see if it can get it right thanks marc

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 
-
 namespace WpfApp1
 {
     /// <summary>
@@ -11,6 +10,15 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double ellipseWidth = 300;
+        private double ellipseHeight = 200;
+        private double maxButtonWidth = 50;
+        private double minButtonWidth = 100;
+        private double maxButtonHeight = 25;
+        private double minButtonHeight = 50;
+        private double ellipseCenterX = 150;
+        private double ellipseCenterY = 100;
+
         private const int NumButtons = 8;
         private Button[] buttons = new Button[NumButtons];
         private double[] angles = new double[NumButtons];
@@ -33,6 +41,7 @@ namespace WpfApp1
                 buttons[i].Click += Button_Click;
                 MoveButtonAlongPath(buttons[i], angles[i]);
             }
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -51,31 +60,30 @@ namespace WpfApp1
 
         private void MoveButtonAlongPath(Button button, double angle)
         {
-            double radiusX = ellipse.Width / 2;
-            double radiusY = ellipse.Height / 2;
-            double centerX = ellipse.Width / 2;
-            double centerY = ellipse.Height / 2;
+            double radiusX = ellipseWidth / 2;
+            double radiusY = ellipseHeight / 2;
 
-            double x = centerX + radiusX * Math.Cos(angle * Math.PI / 180);
-            double y = centerY + radiusY * Math.Sin(angle * Math.PI / 180);
+            double x = ellipseCenterX + radiusX * Math.Cos(angle * Math.PI / 180);
+            double y = ellipseCenterY + radiusY * Math.Sin(angle * Math.PI / 180);
 
             double scale = 1 - (angle % 360) / 360.0;
+            double width = maxButtonWidth - (maxButtonWidth - minButtonWidth) * scale;
+            double height = maxButtonHeight - (maxButtonHeight - minButtonHeight) * scale;
 
-            button.RenderTransform = new TranslateTransform(x - button.Width / 2, y - button.Height / 2);
-            button.LayoutTransform = new ScaleTransform(scale, scale);
+            button.RenderTransform = new TranslateTransform(x - width / 2, y - height / 2);
+            button.Width = width;
+            button.Height = height;
         }
 
         private void AnimateButtonAlongPath(Button button, double fromAngle, double toAngle)
         {
-            double radiusX = ellipse.Width / 2;
-            double radiusY = ellipse.Height / 2;
-            double centerX = ellipse.Width / 2;
-            double centerY = ellipse.Height / 2;
+            double radiusX = ellipseWidth / 2;
+            double radiusY = ellipseHeight / 2;
 
-            double fromX = centerX + radiusX * Math.Cos(fromAngle * Math.PI / 180);
-            double fromY = centerY + radiusY * Math.Sin(fromAngle * Math.PI / 180);
-            double toX = centerX + radiusX * Math.Cos(toAngle * Math.PI / 180);
-            double toY = centerY + radiusY * Math.Sin(toAngle * Math.PI / 180);
+            double fromX = ellipseCenterX + radiusX * Math.Cos(fromAngle * Math.PI / 180);
+            double fromY = ellipseCenterY + radiusY * Math.Sin(fromAngle * Math.PI / 180);
+            double toX = ellipseCenterX + radiusX * Math.Cos(toAngle * Math.PI / 180);
+            double toY = ellipseCenterY + radiusY * Math.Sin(toAngle * Math.PI / 180);
 
             TranslateTransform translateTransform = new TranslateTransform(fromX - button.Width / 2, fromY - button.Height / 2);
             button.RenderTransform = translateTransform;
@@ -86,13 +94,34 @@ namespace WpfApp1
             translateTransform.BeginAnimation(TranslateTransform.XProperty, xAnimation);
             translateTransform.BeginAnimation(TranslateTransform.YProperty, yAnimation);
 
-            ScaleTransform scaleTransform = new ScaleTransform(1, 1);
-            button.LayoutTransform = scaleTransform;
+            DoubleAnimation widthAnimation = new DoubleAnimation(button.Width, maxButtonWidth - (maxButtonWidth - minButtonWidth) * (1 - (toAngle % 360) / 360.0), TimeSpan.FromSeconds(1));
+            DoubleAnimation heightAnimation = new DoubleAnimation(button.Height, maxButtonHeight - (maxButtonHeight - minButtonHeight) * (1 - (toAngle % 360) / 360.0), TimeSpan.FromSeconds(1));
 
-            DoubleAnimation scaleAnimation = new DoubleAnimation(1, 1 - (toAngle % 360) / 360.0, TimeSpan.FromSeconds(1));
-            scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnimation);
-            scaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnimation);
+            button.BeginAnimation(Button.WidthProperty, widthAnimation);
+            button.BeginAnimation(Button.HeightProperty, heightAnimation);
         }
 
+        private void UpdateButtonPosition(Button button, double angle)
+        {
+            double radiusX = ellipseWidth / 2;
+            double radiusY = ellipseHeight / 2;
+
+            double radian = angle * Math.PI / 180;
+
+            double x = radiusX * Math.Cos(radian);
+            double y = radiusY * Math.Sin(radian);
+
+            // Adjust scale calculation to make bottom center (270 degrees) the largest
+            double scale = (Math.Cos(radian - Math.PI / 2) + 1) / 2;
+            double width = minButtonWidth + (maxButtonWidth - minButtonWidth) * scale;
+            double height = minButtonHeight + (maxButtonHeight - minButtonHeight) * scale;
+
+            button.Width = width;
+            button.Height = height;
+
+            Canvas.SetLeft(button, ellipseCenterX + x - width / 2);
+            Canvas.SetTop(button, ellipseCenterY + y - height / 2);
+            Panel.SetZIndex(button, (int)(1000 * (1 - scale))); // Highest Z-index for the bottom button
+        }
     }
 }
