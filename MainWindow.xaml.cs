c829a242e82073f7d8888b82c27ebc7049769bad
CatiaWidgets <122708053+TrueNorthPLM@users.noreply.github.com>
2024-07-26 13:02:19 -0700

    Added a Basic View Model to Show How

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,67 +1,15 @@
-﻿using MahApps.Metro.IconPacks;
-using System.Collections.ObjectModel;
-using System.Windows;
-using WpfApp1.Commands.Base;
-using WpfApp1.Models;
-
+﻿using System.Windows;
+using WpfApp1.ViewModels;
 
 namespace WpfApp1
 {
     public partial class MainWindow : Window
     {
-        public ObservableCollection<CarouselButton> CarouselButtons { get; set; }
+        public CarouselViewModel CarouselViewModel { get; set; } = new CarouselViewModel();
         public MainWindow()
         {
             InitializeComponent();
-
-
-            CarouselButtons = new ObservableCollection<CarouselButton>
-            {
-                new CarouselButton { Icon = PackIconMaterialKind.Abacus, Command = new CommandsBase(abacusExecute) },
-                new CarouselButton { Icon = PackIconMaterialKind.TableAccount, Command = new CommandsBase(tableAccountExecute) },
-                new CarouselButton { Icon = PackIconMaterialKind.Kangaroo, Command = new CommandsBase(kangarooExecute) },
-                new CarouselButton { Icon = PackIconMaterialKind.BabyBuggy, Command = new CommandsBase(babyBuggyExecute) },
-                new CarouselButton { Icon = PackIconMaterialKind.CabinAFrame, Command = new CommandsBase(cabinAFrameExecute) },
-                new CarouselButton { Icon = PackIconMaterialKind.DanceBallroom, Command = new CommandsBase(danceBallRoomExecute) },
-                new CarouselButton { Icon = PackIconMaterialKind.FaceAgent, Command = new CommandsBase(fanceAgentExecute) },
-            };
-
             DataContext = this;
-        }
-
-        private void abacusExecute(object? obj)
-        {
-            MessageBox.Show($"Abacus");
-        }
-
-        private void fanceAgentExecute(object? obj)
-        {
-            MessageBox.Show($"FanceAgent");
-        }
-
-        private void danceBallRoomExecute(object? obj)
-        {
-            MessageBox.Show($"DanceBallRoom");
-        }
-
-        private void cabinAFrameExecute(object? obj)
-        {
-            MessageBox.Show($"CabinAFrame");
-        }
-
-        private void babyBuggyExecute(object? obj)
-        {
-            MessageBox.Show($"BabyBuggy");
-        }
-
-        private void kangarooExecute(object? obj)
-        {
-            MessageBox.Show($"Kangaroo");
-        }
-
-        private void tableAccountExecute(object? obj)
-        {
-            MessageBox.Show($"TableAccount");
         }
     }
 }
--- ViewModels/CarouselViewModel.cs
+++ ViewModels/CarouselViewModel.cs
@@ -3,34 +3,71 @@ using WpfApp1.Models;
 using WpfApp1.ViewModels.Base;
 
 using MahApps.Metro.IconPacks;
+using System.Windows;
+using WpfApp1.Commands.Base;
 
 namespace WpfApp1.ViewModels
 {
-    public class CarouselViewModel:ViewModelBase
+    public class CarouselViewModel : ViewModelBase
     {
-        private ObservableCollection<CarouselButton> _buttons = null;
-        public ObservableCollection<CarouselButton>? Buttons
+        private ObservableCollection<CarouselButton>? _carouselButtons = null;
+        public ObservableCollection<CarouselButton>? CarouselButtons
         {
-            get => _buttons;
+            get => _carouselButtons;
             set
             {
-                _buttons = value;
+                _carouselButtons = value;
                 OnPropertyChanged();
             }
         }
 
         public CarouselViewModel()
         {
-            Buttons = new ObservableCollection<CarouselButton>
+            CarouselButtons = new ObservableCollection<CarouselButton>
             {
-                new CarouselButton { Icon = PackIconMaterialKind.Abacus },
-                new CarouselButton { Icon = PackIconMaterialKind.TableAccount },
-                new CarouselButton { Icon = PackIconMaterialKind.Kangaroo },
-                new CarouselButton { Icon = PackIconMaterialKind.BabyBuggy },
-                new CarouselButton { Icon = PackIconMaterialKind.CabinAFrame },
-                new CarouselButton { Icon = PackIconMaterialKind.DanceBallroom },
-                new CarouselButton { Icon = PackIconMaterialKind.FaceAgent },
+                new CarouselButton { Icon = PackIconMaterialKind.Abacus, Command = new CommandsBase(abacusExecute) },
+                new CarouselButton { Icon = PackIconMaterialKind.TableAccount, Command = new CommandsBase(tableAccountExecute) },
+                new CarouselButton { Icon = PackIconMaterialKind.Kangaroo, Command = new CommandsBase(kangarooExecute) },
+                new CarouselButton { Icon = PackIconMaterialKind.BabyBuggy, Command = new CommandsBase(babyBuggyExecute) },
+                new CarouselButton { Icon = PackIconMaterialKind.CabinAFrame, Command = new CommandsBase(cabinAFrameExecute) },
+                new CarouselButton { Icon = PackIconMaterialKind.DanceBallroom, Command = new CommandsBase(danceBallRoomExecute) },
+                new CarouselButton { Icon = PackIconMaterialKind.FaceAgent, Command = new CommandsBase(fanceAgentExecute) },
             };
+        }
+
+        private void abacusExecute(object? obj)
+        {
+            MessageBox.Show($"Abacus");
+        }
+
+        private void fanceAgentExecute(object? obj)
+        {
+            MessageBox.Show($"FanceAgent");
+        }
+
+        private void danceBallRoomExecute(object? obj)
+        {
+            MessageBox.Show($"DanceBallRoom");
+        }
+
+        private void cabinAFrameExecute(object? obj)
+        {
+            MessageBox.Show($"CabinAFrame");
+        }
+
+        private void babyBuggyExecute(object? obj)
+        {
+            MessageBox.Show($"BabyBuggy");
+        }
+
+        private void kangarooExecute(object? obj)
+        {
+            MessageBox.Show($"Kangaroo");
+        }
+
+        private void tableAccountExecute(object? obj)
+        {
+            MessageBox.Show($"TableAccount");
         }
     }
 }
