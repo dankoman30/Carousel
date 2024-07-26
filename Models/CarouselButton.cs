8cc7f6d9cb8c8ad2f4b9d31cf90af8f60b579ea5
CatiaWidgets <122708053+TrueNorthPLM@users.noreply.github.com>
2024-07-26 12:51:38 -0700

    Execute Correct ICommand For Active Button

--- Models/CarouselButton.cs
+++ Models/CarouselButton.cs
@@ -1,9 +1,11 @@
 ﻿using MahApps.Metro.IconPacks;
+using System.Windows.Input;
 
 namespace WpfApp1.Models
 {
     public class CarouselButton
     {
         public PackIconMaterialKind Icon { get; set; } = PackIconMaterialKind.None;
+        public ICommand? Command { get; set; } = null;
     }
 }
